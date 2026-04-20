700f86f11e7a8599d874df4a28e9f857440f3aba
Arnav <arnavuniyal594@gmail.com>
2026-04-20 23:28:06 +0530

    files uploaded

--- frontend/src/components/StationSelector.jsx
+++ frontend/src/components/StationSelector.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "../App.css";
 
-const API_URL = "https://delhi-transit-analysis-system.onrender.com";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function StationSelector() {
   const [mode, setMode] = useState("metro");
